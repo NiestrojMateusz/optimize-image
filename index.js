4f39397fb6f209edccdce3d4f55c99ac74309c64
Mateusz Niestrój <mateusz.niestroj@altimi.com>
2020-12-19 13:25:06 +0100

    📦 NEW: Initial version

---
+++ index.js
@@ -0,0 +1,75 @@
+#!/usr/bin/env node
+
+/**
+ * optimize-img
+ * Node CLI app to batch images optimization
+ *
+ * @author Mateusz Niestrój <https://github.com/NiestrojMateusz>
+ */
+
+const Jimp = require('jimp');
+const globby = require('globby');
+const alert = require('cli-alerts');
+
+const init = require('./utils/init');
+const cli = require('./utils/cli');
+const log = require('./utils/log');
+const { Confirm } = require('enquirer');
+
+const input = cli.input;
+const flags = cli.flags;
+const { clear, debug, source, quality, output } = flags;
+
+(async () => {
+  init({ clear });
+  input.includes(`help`) && cli.showHelp(0);
+
+  if (source) {
+    if (!output) {
+      const prompt = new Confirm({
+        name: 'Overwrite confirm',
+        message: 'Are you sure you want to overwrite source files?'
+      });
+
+      const answer = await prompt
+        .run()
+        .then(answer => {
+          console.log('Answer:', answer ? 'Yes' : 'No');
+          return answer;
+        })
+        .catch(console.error);
+
+      if (!answer) {
+        alert({
+          type: `error`,
+          name: `No overwrite`,
+          msg: `You forgot to specify --output flag. No overwirte`
+        });
+      }
+    }
+
+    const images = await globby(source);
+
+    const options = {
+      images,
+      qulaity: quality ? +quality : 90,
+      output
+    };
+
+    await Promise.all(
+      options.images.map(async imgPath => {
+        const image = await Jimp.read(imgPath);
+        await image.quality(+options.quality);
+        await image.writeAsync(`${output}/${imgPath}`);
+      })
+    );
+  } else {
+    alert({
+      type: `error`,
+      name: `No source error`,
+      msg: `You forgot to specify --source flag`
+    });
+  }
+
+  debug && log(flags);
+})();
---
+++ utils/cli.js
@@ -0,0 +1,61 @@
+const meow = require('meow');
+const meowHelp = require('cli-meow-help');
+
+const flags = {
+  output: {
+    type: 'string',
+    alias: 'o',
+    desc: 'Path to output dir'
+  },
+  source: {
+    type: `string`,
+    alias: `s`,
+    desc: `Input file or directory of images`
+  },
+  quality: {
+    type: `string`,
+    alias: `q`,
+    desc: `Quality of output images`
+  },
+  clear: {
+    type: `boolean`,
+    default: true,
+    alias: `c`,
+    desc: `Clear the console`
+  },
+  'no-clear': {
+    type: `boolean`,
+    default: false,
+    desc: `Don't clear the console`
+  },
+  debug: {
+    type: `boolean`,
+    default: false,
+    alias: `d`,
+    desc: `Print debug info`
+  },
+  version: {
+    type: `boolean`,
+    alias: `v`,
+    desc: `Print CLI version`
+  }
+};
+
+const commands = {
+  help: { desc: `Print help info` }
+};
+
+const helpText = meowHelp({
+  name: `optimize-img`,
+  flags,
+  commands
+});
+
+const options = {
+  inferType: true,
+  description: false,
+  hardRejection: false,
+  flags
+};
+
+module.exports = meow(helpText, options);
